b3b98fa16dc5298a302af6bc68d2bfa0ad48e1b6
aparimeet <aparimeet@gmail.com>
2020-01-21 18:39:22 +0530

    Add files via upload

---
+++ OddEvenGame.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.21;
+
+contract OddEvenGame {
+    struct Player{
+        address addr;
+        uint number;
+    }
+    Player[2] private players;
+    uint8 count = 0;
+    
+    function play(uint number) payable public {
+        require(msg.value == 1 ether);
+        players[count] = Player(msg.sender, number);
+        count++;
+        if (count == 2) selectWinner();
+    }
+    
+    function selectWinner() private {
+        uint n = players[0].number + players[1].number;
+        players[n % 2].addr.transfer(address(this).balance);
+        delete players;
+        count = 0;
+    }
+}
